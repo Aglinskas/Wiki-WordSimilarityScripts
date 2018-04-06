03814d7ac166852b6835769df973e23920baea1d
Aidas Aglinskas <aidasaglinskas@Aidas-Aglinskas-Macbook-Pro.local>
2018-04-06 18:05:51 +0200

    code edits

--- func_wiki_addSimMats.m
+++ func_wiki_addSimMats.m
@@ -5,10 +5,10 @@ tic
         warning('on');warning('no input, loading cached wiki')
         load('/Users/aidasaglinskas/Desktop/Wiki-wordSimilarityScripts/Jwiki.mat')
         pause(1)
-        thresh = [1 1]
+        thresh = [1 1];
     elseif nargin == 1
         wiki = varargin{1};
-        thresh = [1 1]
+        thresh = [1 1];
     elseif nargin == 2
         wiki = varargin{1};
         thresh = varargin{2};
@@ -22,13 +22,16 @@ wiki.sim_feat = corr(wiki.dm_avg);
 wiki.sim_noun = corr(wiki.dm_avg');
 %clc;disp(wiki)
 %% Cluster
-mats = {wiki.dm_avg wiki.dm_avg'};
+%mats = {wiki.dm_avg wiki.dm_avg'};
+mats = {wiki.sim_noun wiki.sim_feat};
 lbls = {wiki.nouns wiki.featwords};
-ttls = {'Noun Clustering' 'Feature Clustering'}
+ttls = {'Noun Clustering' 'Feature Clustering'};
 for w_mat = 1:2;
+disp(sprintf('%d/2',w_mat));
     figure(w_mat);
-d = pdist(mats{w_mat},'correlation');
-Z = linkage(d,'ward');
+%d = pdist(mats{w_mat},'correlation');
+%Z = linkage(d,'ward');
+Z = linkage(1-get_triu(mats{w_mat}),'ward');
     figure(w_mat);
     [h{w_mat} x{w_mat} perm{w_mat}] = dendrogram(Z,0,'labels',lbls{w_mat},'colorthreshold',thresh(w_mat));
     xtickangle(45)
@@ -46,6 +49,6 @@ n_clust = length(unique(cellfun(@num2str,{h{w_mat}.Color},'UniformOutput',0)));
         wiki.feat_ord = perm{w_mat}';
     end
 figure(w_mat);
-title({ttls{w_mat} ['n-clust ' num2str(n_clust) ', n-items ' num2str(size(wiki.dm_avg,1)) ', thresh ' num2str(thresh(w_mat))]},'fontsize',20)
+title({ttls{w_mat} ['n-clust ' num2str(n_clust) ', n-items ' num2str(length(mats{w_mat})) ', thresh ' num2str(thresh(w_mat))]},'fontsize',20)
 end
 toc
